fa4a83615e74e1c353121694062f43a0e0bab2ee
SumanRout <sumanrout824@gmail.com>
2025-07-31 12:22:55 +0530

    Backend,database connected but not completed,frontend also

---
+++ server/server.js
@@ -0,0 +1,27 @@
+const express=require('express')
+const mongoose=require('mongoose')
+const cookieParser=require('cookie-parser')
+const cors=require('cors')
+mongoose.connect('mongodb+srv://sumanrout824:vPWjDrmyWqzXx38ht@cluster0.hcdidj4.mongodb.net/'
+
+).then(()=>console.log("Mongodb connected")).catch((error)=>console.log(error))
+
+const app=express()
+const PORT=process.env.PORT ||5000
+app.use(
+    cors({
+        origin:'http://localhost:5173/',
+        method:['GET','POST','DELETE','PUT'],
+        allowedHeaders:[
+            "Content-Type",
+            'Authorization',
+            'Cache-Control',
+            'Expires',
+            'pragma'
+        ],
+        credentials:true
+    })
+)
+app.use(cookieParser())
+app.use(express.json())
+app.listen(PORT,()=>console.log(`Server running on port ${PORT}`))
